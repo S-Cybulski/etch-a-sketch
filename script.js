4bc89a95bc7c399aed4d8816d3926476db8840f0
S-Cybulski <szymcyb04@protonmail.com>
2023-09-13 22:01:46 +0100

    Added ability to change canvas size
    Change canvas size by altering divWidth

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < 16; i++){
+let size = 32;
+let divWidth = (32/(Math.round(size/16))).toString();
+
+for (let i = 0; i < size; i++){
     const column = document.createElement('div')
     column.classList.add('column')
-    for (let i = 0; i < 16; i++){
+    for (let i = 0; i < size; i++){
         let gridSquare = document.createElement('div');
         gridSquare.classList.add('gridSquare');
+        gridSquare.style.cssText =  `width: ${divWidth}px; height: ${divWidth}px;`;
         column.appendChild(gridSquare);
     }
     grid.appendChild(column);
@@ -15,7 +19,7 @@ const gridSquares = Array.from(document.querySelectorAll('.gridSquare'));
 
 gridSquares.forEach(square => { 
     square.addEventListener('mouseover', () => {
-        square.style = 'background-color: black;'
+        square.style.background = 'black';
     })
 });
 

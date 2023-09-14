3ce3dba38e1a78a13b03959940b07154d02e5f35
S-Cybulski <szymcyb04@protonmail.com>
2023-09-14 14:57:36 +0100

    added border around the canvas
    added border and removed grid square styling

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 const grid = document.querySelector('.grid');
 
-let size = 32;
-let divWidth = (32/(Math.round(size/16))).toString();
+let size = 64;
+let divWidth = (480/size).toString();
 
-for (let i = 0; i < size; i++){
-    const column = document.createElement('div')
-    column.classList.add('column')
+function createGrid(){
+
     for (let i = 0; i < size; i++){
-        let gridSquare = document.createElement('div');
-        gridSquare.classList.add('gridSquare');
-        gridSquare.style.cssText =  `width: ${divWidth}px; height: ${divWidth}px;`;
-        column.appendChild(gridSquare);
+        const column = document.createElement('div')
+        column.classList.add('column')
+        for (let i = 0; i < size; i++){
+            let gridSquare = document.createElement('div');
+            gridSquare.classList.add('gridSquare');
+            gridSquare.style.cssText =  `width: ${divWidth}px; height: ${divWidth}px;`;
+            column.appendChild(gridSquare);
+        }
+        grid.appendChild(column);
     }
-    grid.appendChild(column);
+
+    const gridSquares = Array.from(document.querySelectorAll('.gridSquare'));
+
+    gridSquares.forEach(square => { 
+        square.addEventListener('mouseover', () => {
+            square.style.background = 'black';
+        })
+    });
+
 }
 
-const gridSquares = Array.from(document.querySelectorAll('.gridSquare'));
-
-gridSquares.forEach(square => { 
-    square.addEventListener('mouseover', () => {
-        square.style.background = 'black';
-    })
-});
-
 function resetGrid(){
+
+    const gridSquares = Array.from(document.querySelectorAll('.gridSquare'));
+    
     gridSquares.forEach(square => {
         square.style.background = 'white';
     });
-};
 
+}
+
+createGrid();

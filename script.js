a25e71ba96218077b3c0dba677ca3e832ec2ce4c
S-Cybulski <szymcyb04@protonmail.com>
2023-09-14 19:01:57 +0100

    added rainbow mode and resize grid
    added a toggle rainbow mode button and added resize grid prompt

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const grid = document.querySelector('.grid');
 
-let size = 64;
+let size = 32;
 let divWidth = (480/size).toString();
+let rainbow = false;
 
 function createGrid(){
 
     for (let i = 0; i < size; i++){
-        const column = document.createElement('div')
-        column.classList.add('column')
+        const column = document.createElement('div');
+        column.classList.add('column');
         for (let i = 0; i < size; i++){
             let gridSquare = document.createElement('div');
             gridSquare.classList.add('gridSquare');
@@ -19,9 +20,17 @@ function createGrid(){
 
     const gridSquares = Array.from(document.querySelectorAll('.gridSquare'));
 
-    gridSquares.forEach(square => { 
+    gridSquares.forEach(square => {
         square.addEventListener('mouseover', () => {
-            square.style.background = 'black';
+            if (rainbow){
+                let red = ((Math.random() * 255) + 1).toString()
+                let green = ((Math.random() * 255) + 1).toString()
+                let blue = ((Math.random() * 255) + 1).toString()
+                square.style.background = `rgb(${red},${green},${blue})`;
+            }
+            else{
+                square.style.background = 'black';
+            }
         })
     });
 
@@ -34,7 +43,24 @@ function resetGrid(){
     gridSquares.forEach(square => {
         square.style.background = 'white';
     });
+    
+}
 
+function recreateGrid(){
+    const gridSquares = Array.from(document.querySelectorAll('.gridSquare'))
+
+    gridSquares.forEach(square => {
+        square.remove();
+    })
+    
+    size = (window.prompt("Enter Grid Size:"))%100;
+    divWidth = (480/size).toString();
+
+    createGrid();
+}
+
+function rainbowMode(){
+    rainbow = !(rainbow);
 }
 
 createGrid();
